b32752dbd69ea84bc429ee0072a07a21795bfc3a
robsoninoue <robson.inoue@gmail.com>
2023-06-07 16:35:02 -0300

    created conditionals in header to work in linux and mac

--- src/include/minirt.h
+++ src/include/minirt.h
@@ -6,7 +6,7 @@
 /*   By: rotakesh <rotakesh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 21:38:20 by rotakesh          #+#    #+#             */
-/*   Updated: 2023/06/07 06:55:59 by coder            ###   ########.fr       */
+/*   Updated: 2023/06/07 16:30:23 by rotakesh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,15 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# ifdef __APPLE__
+# include "mlx.h" // delete before close the project
+# elif __linux__
 # include <mlx.h> // needed to run in 42's linux
 # include <X11/X.h> // needed to run in 42's linux
 # include <X11/keysym.h> // needed to run in 42's linux
+# endif
 # include <math.h>
 # include "defines.h"
 # include "structs.h"
-//# include "mlx.h" // delete before close the project
 
 #endif
