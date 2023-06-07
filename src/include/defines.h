c4b43f6009fa16e56bac8e694fd1445b0b513ba5
robsoninoue <robson.inoue@gmail.com>
2023-06-07 01:46:37 -0300

    Headers | created defines and structs headers and updated minirt.h

--- src/include/defines.h
+++ src/include/defines.h
@@ -1,29 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minirt.h                                           :+:      :+:    :+:   */
+/*   defines.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rotakesh <rotakesh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/06 21:38:20 by rotakesh          #+#    #+#             */
-/*   Updated: 2023/06/07 01:44:55 by rotakesh         ###   ########.fr       */
+/*   Created: 2023/06/06 21:44:14 by rotakesh          #+#    #+#             */
+/*   Updated: 2023/06/07 01:15:17 by rotakesh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINIRT_H
-# define MINIRT_H
+#ifndef DEFINES_H
+# define DEFINES_H
 
-# define VERSION "0.0.1"
+# include "minirt.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-// # include <mlx.h> // needed to run in 42's linux
-// # include <X11/X.h> // needed to run in 42's linux
-// # include <X11/keysym.h> // needed to run in 42's linux
-# include <math.h>
-# include "defines.h"
-# include "structs.h"
-# include "mlx.h" // delete before close the project
+# define WIDTH 640
+# define HEIGHT 480
 
 #endif
---
+++ src/include/structs.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rotakesh <rotakesh@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/06 21:39:08 by rotakesh          #+#    #+#             */
+/*   Updated: 2023/06/07 01:15:34 by rotakesh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTS_H
+# define STRUCTS_H
+
+# include "minirt.h"
+
+typedef struct s_img
+{
+	void	*ptr;
+	void	*addr;
+	int		bits_per_pixel;
+	int		line_lenght;
+	int		endian;
+}	t_img;
+
+typedef struct s_screen
+{
+	double	x[WIDTH];
+	double	y[HEIGHT];
+	double	initx;
+	double	inity;
+	double	zoom;
+	double	passx;
+	double	passy;
+}	t_screen;
+
+typedef struct s_data {
+	void		*mlx;
+	void		*win;
+	char		*win_name;
+	int			win_w;
+	int			win_h;
+	t_img		img;
+	t_screen	screen;
+}	t_data;
+
+#endif
