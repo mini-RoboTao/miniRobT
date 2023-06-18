fb45348f04e7acf7dfd8ad1f219f1c218a619684
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-06-18 16:54:13 -0300

    feat(libft): Add functions to libft: Add the functions ft_atol, ft_islower, ft_isspace, ft_isupper in the libft

--- src/libs/libft/libft.h
+++ src/libs/libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rotakesh <rotakesh@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: dapaulin <dapaulin@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 23:31:49 by rotakesh          #+#    #+#             */
-/*   Updated: 2022/01/19 15:09:40 by rotakesh         ###   ########.fr       */
+/*   Updated: 2023/06/18 16:49:38 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,39 +16,47 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int		ft_atoi(const char *str);
-void	ft_bzero(void *b, size_t n);
-void	*ft_calloc(size_t n, size_t size);
+// Base Functions
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
+int		ft_isspace(int c);
 int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_islower(int c);
+int		ft_isupper(int c);
+size_t	ft_strlen(const char *str);
+// Print Functions
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+// Handle Numbers or Strings
+int		ft_atoi(const char *str);
+long	ft_atol(const char *nptr);
 char	*ft_itoa(int n);
+// Handle memory
+void	ft_bzero(void *b, size_t n);
+void	*ft_calloc(size_t n, size_t size);
 void	*ft_memchr(const void *str, int c, size_t n);
 int		ft_memcmp(const void *str1, const void *str2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *str1, const void *str2, size_t n);
 void	*ft_memset(void *dest, int value, size_t len);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putstr_fd(char *s, int fd);
+// Alter strings
 char	**ft_split(char const *s, char c);
+char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
 char	*ft_strdup(const char *s);
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
-char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-size_t	ft_strlen(const char *str);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *str, const char *to_find, size_t len);
-char	*ft_strrchr(const char *str, int c);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-
 #endif
