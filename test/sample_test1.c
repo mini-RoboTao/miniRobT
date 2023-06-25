075edf9a17d6fa4082bd0c02acdf99bd4ffcde17
rotakesh <rotakesh@student.42sp.org.br>
2023-06-25 03:13:54 -0300

    fixed errors in tests to assert

--- test/sample_test1.c
+++ test/sample_test1.c
@@ -7,8 +7,8 @@
 Test(sample, test)
 {
     cr_expect(strlen("Test") == 4, "Expected \"Test\" to have a length of 4.");
-    cr_expect(strlen("Hello") == 4, "This will always fail, why did I add this?");
-    // cr_assert(strlen("") == 0);
+    // cr_expect(strlen("Hello") == 4, "This will always fail, why did I add this?");
+    cr_assert(strlen("") == 0);
 }
 
 Test(sample, test2)
