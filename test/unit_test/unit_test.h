33e8d359be322c613e006cbfb81377564b59d349
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-06-29 11:02:29 -0300

    test(create arithmetic functions create test for the functions sum, subtract, negate, multiply e divide alter Makefile for build the tests files

--- test/unit_test/unit_test.h
+++ test/unit_test/unit_test.h
@@ -5,4 +5,21 @@
 # include "../criterion-2.4.2/include/criterion/criterion.h"
 # include "../criterion-2.4.2/include/criterion/new/assert.h"
 
+void	clean_obj(t_obj *o)
+{
+	if (o)
+	{
+		free(o);
+		o = NULL;
+	}
+}
+
+int cr_object_eq(t_obj *a, t_obj *b)
+{
+    return	(fabs(a->x - b->x) < EPSILON && \
+			fabs(a->y - b->y) < EPSILON && \
+			fabs(a->z - b->z) < EPSILON && \
+			a->w == b->w);
+}
+
 #endif
