e89f1b5f2a9a9f41cabe457624f505e3dee09967
rotakesh <rotakesh@student.42sp.org.br>
2023-07-12 20:36:30 -0300

    fix rotation_y

--- src/include/matrix_transformations.h
+++ src/include/matrix_transformations.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   matrix_transformations.h                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rotakesh <rotakesh@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: rotakesh <rotakesh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 11:19:10 by rotakesh          #+#    #+#             */
-/*   Updated: 2023/07/12 09:30:45 by rotakesh         ###   ########.fr       */
+/*   Updated: 2023/07/12 20:34:57 by rotakesh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 int	translation(t_matrix *matrix, double x, double y, double z);
 int	scaling(t_matrix *matrix, double x, double y, double z);
 int	rotation_x(t_matrix *matrix, double r);
+int	rotation_y(t_matrix *matrix, double r);
 
 #endif
