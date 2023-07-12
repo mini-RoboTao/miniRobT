b937e63b5e4a42b4c48978d275ddf3c63f39a0f5
Robson Inoue <robson.inoue@gmail.com>
2023-07-12 19:04:54 -0300

    WIP matrix rotations

--- src/matrix_transformations/rotation_matrix.c
+++ src/matrix_transformations/rotation_matrix.c
@@ -1,22 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   matrix_transformations.h                           :+:      :+:    :+:   */
+/*   rotation_matrix.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rotakesh <rotakesh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/11 11:19:10 by rotakesh          #+#    #+#             */
-/*   Updated: 2023/07/12 09:30:45 by rotakesh         ###   ########.fr       */
+/*   Created: 2023/07/11 11:18:17 by rotakesh          #+#    #+#             */
+/*   Updated: 2023/07/12 09:29:32 by rotakesh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MATRIX_TRANSFORMATIONS_H
-# define MATRIX_TRANSFORMATIONS_H
+#include "minirt.h"
 
-# include "minirt.h"
-
-int	translation(t_matrix *matrix, double x, double y, double z);
-int	scaling(t_matrix *matrix, double x, double y, double z);
-int	rotation_x(t_matrix *matrix, double r);
-
-#endif
+int	rotation_x(t_matrix *matrix, double r)
+{
+	if (!matrix)
+		return (-1);
+	if (matrix->x != 4 || matrix->y != 4)
+		return (-1);
+	matrix->node[0][0] = 1;
+	matrix->node[1][1] = cos(r);
+	matrix->node[1][2] = sin(r) * -1;
+	matrix->node[2][1] = sin(r);
+	matrix->node[2][2] = cos(r);
+	matrix->node[3][3] = 1;
+	return (1);
+}
