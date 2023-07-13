550d626eace3caee348f1ceabb3351d26a9bef22
Robson Inoue <robson.inoue@gmail.com>
2023-07-12 23:40:06 -0300

    wip | added rotation y and z

--- src/include/matrix_transformations.h
+++ src/include/matrix_transformations.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   matrix_transformations.h                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rotakesh <rotakesh@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rotakesh <rotakesh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 11:19:10 by rotakesh          #+#    #+#             */
-/*   Updated: 2023/07/12 20:34:57 by rotakesh         ###   ########.fr       */
+/*   Updated: 2023/07/12 23:34:36 by rotakesh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@
 
 # include "minirt.h"
 
+// TRANSLATION
 int	translation(t_matrix *matrix, double x, double y, double z);
+
+// SCALING
 int	scaling(t_matrix *matrix, double x, double y, double z);
+
+// ROTATION
 int	rotation_x(t_matrix *matrix, double r);
 int	rotation_y(t_matrix *matrix, double r);
+int	rotation_z(t_matrix *matrix, double r);
 
 #endif
--- src/matrix_transformations/rotation_matrix.c
+++ src/matrix_transformations/rotation_matrix.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rotation_matrix.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rotakesh <rotakesh@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rotakesh <rotakesh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 11:18:17 by rotakesh          #+#    #+#             */
-/*   Updated: 2023/07/12 20:34:45 by rotakesh         ###   ########.fr       */
+/*   Updated: 2023/07/12 23:32:36 by rotakesh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,26 @@ int	rotation_y(t_matrix *matrix, double r)
 		return (-1);
 	if (matrix->x != 4 || matrix->y != 4)
 		return (-1);
-	matrix->node[0][0] = 1;
-	matrix->node[1][1] = cos(r);
-	matrix->node[1][2] = sin(r) * -1;
-	matrix->node[2][1] = sin(r);
+	matrix->node[0][0] = cos(r);
+	matrix->node[0][2] = sin(r);
+	matrix->node[1][1] = 1;
+	matrix->node[2][0] = sin(r) * -1;
 	matrix->node[2][2] = cos(r);
+	matrix->node[3][3] = 1;
+	return (1);
+}
+
+int	rotation_z(t_matrix *matrix, double r)
+{
+	if (!matrix)
+		return (-1);
+	if (matrix->x != 4 || matrix->y != 4)
+		return (-1);
+	matrix->node[0][0] = cos(r);
+	matrix->node[0][1] = sin(r) * -1;
+	matrix->node[1][0] = sin(r);
+	matrix->node[1][1] = cos(r);
+	matrix->node[2][2] = 1;
 	matrix->node[3][3] = 1;
 	return (1);
 }
