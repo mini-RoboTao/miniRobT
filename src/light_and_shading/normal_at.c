8099850ff3f628e19bbbfd6969a0b3305a292355
rotakesh <rotakesh@student.42sp.org.br>
2023-07-19 00:34:06 -0300

    wip | added reflections tests

--- src/light_and_shading/normal_at.c
+++ src/light_and_shading/normal_at.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   normal_at.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rotakesh <rotakesh@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: rotakesh <rotakesh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 21:49:46 by rotakesh          #+#    #+#             */
-/*   Updated: 2023/07/18 23:56:10 by rotakesh         ###   ########.fr       */
+/*   Updated: 2023/07/19 00:18:22 by rotakesh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,21 @@ t_obj	*normal_at(t_sphere *sphere, t_obj *point)
 
 // t_obj	*normal_at(t_sphere *sphere, t_obj *world_point)
 // {
-// 	t_obj	*vec;
+// 	t_obj		*object_point;
+// 	t_obj		*object_normal;
+// 	t_obj		*world_normal;
+// 	t_matrix	*inverse;
+// 	t_obj		*point;
 
-// 	vec = create_vector(point->x - sphere->x, point->y - sphere->y,
-// 			point->z - sphere->z);
-// 	return (object_normalize(vec));
+// 	inverse = inverse_matrix(sphere->transform);
+// 	object_point = multiply_matrix_by_obj(inverse, world_point, 4, 4);
+// 	point = create_point(0, 0, 0);
+// 	object_normal = subtract_objects(object_point, point);
+// 	world_normal = multiply_matrix_by_obj(inverse, object_normal, 4, 4);
+// 	world_normal->w = 0;
+// 	clean_obj(object_point);
+// 	clean_obj(object_normal);
+// 	clean_matrix(inverse);
+// 	clean_obj(point);
+// 	return (object_normalize(world_normal));
 // }
---
+++ test/unit_test/light_and_shading_test/reflecting_vectors_test.c
@@ -0,0 +1,31 @@
+// #include "unit_test.h"
+
+// Test(Reflecting_vectors, Reflecting_a_vector_approaching_at_45_degree)
+// {
+// 	t_obj	*vector = create_vector(1, -1, 0);
+// 	t_obj	*n = create_vector(0, 1, 0);
+// 	t_obj	*r = reflect(vector, n);
+// 	t_obj	*res = create_vector(1, 1, 0);
+
+// 	cr_assert(cr_object_eq(r, res));
+
+// 	clean_obj(vector);
+// 	clean_obj(n);
+// 	clean_obj(r);
+// 	clean_obj(res);
+// }
+
+// Test(Reflecting_vector, Reflecting_a_vector_off_a_slanted_surface)
+// {
+// 	t_obj	*vector = create_vector(0, -1, 0);
+// 	t_obj	*n = create_vector(sqrt(2)/2, sqrt(2)/2, 0);
+// 	t_obj	*r = reflect(vector, n);
+// 	t_obj	*res = create_vector(1, 0, 0);
+
+// 	cr_assert(cr_object_eq(r, res));
+
+// 	clean_obj(vector);
+// 	clean_obj(n);
+// 	clean_obj(r);
+// 	clean_obj(res);
+// }
