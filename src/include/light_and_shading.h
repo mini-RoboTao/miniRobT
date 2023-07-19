d1ce65a33271bc1bc91063bfb7bd3f8960ee28d0
Robson Inoue <robson.inoue@gmail.com>
2023-07-18 23:56:16 -0300

    wip | light and shadding - transform functions

--- src/include/light_and_shading.h
+++ src/include/light_and_shading.h
@@ -6,7 +6,7 @@
 /*   By: rotakesh <rotakesh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 21:50:03 by rotakesh          #+#    #+#             */
-/*   Updated: 2023/07/18 12:08:35 by rotakesh         ###   ########.fr       */
+/*   Updated: 2023/07/18 19:32:20 by rotakesh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define LIGHT_AND_SHADING_H
 
 # include "minirt.h"
+
+t_obj	*normal_at(t_sphere *sphere, t_obj *point);
 
 #endif
--- src/light_and_shading/normal_at.c
+++ src/light_and_shading/normal_at.c
@@ -3,16 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   normal_at.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rotakesh <rotakesh@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rotakesh <rotakesh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 21:49:46 by rotakesh          #+#    #+#             */
-/*   Updated: 2023/07/17 21:56:02 by rotakesh         ###   ########.fr       */
+/*   Updated: 2023/07/18 23:56:10 by rotakesh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-// t_obj	*normal_at(t_sphere sphere, t_obj point)
+	// point->x = point->x - sphere->x;
+	// point->y = point->y - sphere->y;
+	// point->z = point->z - sphere->z;
+
+t_obj	*normal_at(t_sphere *sphere, t_obj *point)
+{
+	t_obj	*vec;
+
+	vec = create_vector(point->x - sphere->x, point->y - sphere->y,
+			point->z - sphere->z);
+	return (object_normalize(vec));
+}
+
+// t_obj	*normal_at(t_sphere *sphere, t_obj *world_point)
 // {
-// 	return (object_normalize());
+// 	t_obj	*vec;
+
+// 	vec = create_vector(point->x - sphere->x, point->y - sphere->y,
+// 			point->z - sphere->z);
+// 	return (object_normalize(vec));
 // }
