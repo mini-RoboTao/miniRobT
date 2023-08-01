f34de708e64d2ed0aa08205032d4f16eefbec73f
Robson Inoue <robson.inoue@gmail.com>
2023-08-01 01:21:51 -0300

    wip fixed object actions test

--- test/unit_test/object_feature_test/object_actions_test.c
+++ test/unit_test/object_feature_test/object_actions_test.c
@@ -1,10 +1,10 @@
 #include "unit_test.h"
 
-const t_environment		expected	=	(t_environment){ \
+t_environment			expected	=	(t_environment){ \
 									.gravity = (t_obj){.w=0, .x=1, .y=2, .z=3}, \
 									.wind = (t_obj){.w=0, .x=1, .y=2, .z=3}};
 
-const	t_projectile	exp_res	=	(t_projectile){ \
+t_projectile			exp_res	=	(t_projectile){ \
 									.position = (t_obj){.w=1, .x=1, .y=2, .z=3}, \
 									.velocity = (t_obj){.w=0, .x=1, .y=2, .z=3}};
 
