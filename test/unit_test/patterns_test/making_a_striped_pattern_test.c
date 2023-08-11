157b760240d9fbbe0b30aebda41649d758200747
rotakesh <rotakesh@student.42sp.org.br>
2023-08-10 22:44:17 -0300

    fixed test making a striped pattern test

--- test/unit_test/patterns_test/making_a_striped_pattern_test.c
+++ test/unit_test/patterns_test/making_a_striped_pattern_test.c
@@ -83,7 +83,7 @@ Test(Making_a_striped_pattern, Lighting_with_a_pattern_applied)
 	lig2.shape = new_sphere();
 
 	t_color		c1 = lighting(lig1, create_point(0, 0, 0));
-	t_color		c2 = lighting(lig2, create_point(0, 0, 0));
+	t_color		c2 = lighting(lig2, create_point(1, 1, 1));
 
 	cr_expect(cr_color_eq(c1, white));
 	cr_expect(cr_color_eq(c2, black));
